9f71bfb2efd957de5094d2527d0d870c35eeb218
AlinaIshtuganova5 <dishtuganova@gmail.com>
2019-06-12 09:26:10 +0300

    Update FullBodyIKUnityChan.cs

--- Unity App/Assets/Extensions/Scripts/FullBodyIKUnityChan.cs
+++ Unity App/Assets/Extensions/Scripts/FullBodyIKUnityChan.cs
@@ -1,7 +1,4 @@
-// Copyright (c) 2016 Nora
-// Released under the "Unity-Chan" license
-// http://unity-chan.com/contents/license_en/
-// http://unity-chan.com/contents/license_jp/
+
 
 using UnityEngine;
 
